f0919123420a95e6d2d8908cc3f5e8dc0ae7ebb5
faiz425 <faizanakbar554@gmail.com>
2026-09-08 15:04:50 +0100

    Add Terraform state permissions to GitHub Actions role

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -24,3 +24,40 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_role_policy" "github_actions_terraform_state" {
+  name = "threatlens-terraform-state-access"
+  role = "threatlens-github-actions-role"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "TerraformStateBucketList"
+        Effect = "Allow"
+
+        Action = [
+          "s3:ListBucket"
+        ]
+
+        Resource = "arn:aws:s3:::threatlens-terraform-state-113462084471"
+      },
+      {
+        Sid    = "TerraformStateObjects"
+        Effect = "Allow"
+
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+
+        Resource = [
+          "arn:aws:s3:::threatlens-terraform-state-113462084471/threatlens/terraform.tfstate",
+          "arn:aws:s3:::threatlens-terraform-state-113462084471/threatlens/terraform.tfstate.tflock"
+        ]
+      }
+    ]
+  })
+}
