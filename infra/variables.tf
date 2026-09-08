57e199716a3aae44cd6ac87d0de2a9fe670e444e
faiz425 <faizanakbar554@gmail.com>
2026-09-08 13:59:05 +0100

    Remove unused Terraform container image variable

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,9 +3,3 @@ variable "aws_region" {
   type        = string
   default     = "eu-west-2"
 }
-
-variable "container_image" {
-  description = "Docker image URI to deploy to ECS."
-  type        = string
-  default     = "113462084471.dkr.ecr.eu-west-2.amazonaws.com/threatlens:latest"
-}
